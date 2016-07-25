20ee28a84fdeb25dd529def1d9fe9117d214daac
John Conti <jhconti@icloud.com>
2016-07-24 22:36:01 -0600

    Time-based sine wave in two colors.

--- src/cljs/sinewave/core.cljs
+++ src/cljs/sinewave/core.cljs
@@ -1,9 +1,44 @@
-(ns sinewave.core)
+(ns sinewave.core
+  (:refer-clojure :exclude [time])
+  (:require cljsjs.rx))
 
 (enable-console-print!)
-(.log js/console "hello clojurescript 2")
+;(.log js/console "hello clojurescript 2")
+
 (def canvas (.getElementById js/document "myCanvas"))
 (def ctx    (.getContext canvas "2d"))
+
+(def interval js/Rx.Observable.interval)
+(def time (interval 10))
+
+(defn deg-to-rad [n]
+  (* (/ Math/PI 180) n))
+
+(defn sine-coord [x]
+  (let [sin (Math/sin (deg-to-rad x))
+        y (- 100 (* sin 90))]
+      {:x x
+       :y y
+       :sin sin}))
+
+(def sine-wave
+  (.map time sine-coord))
+
+(def colour (.map sine-wave
+                  (fn [{:keys [sin]}]
+                    (if (< sin 0)
+                      "red"
+                      "blue"))))
+
+(defn fill-rect [x y colour]
+     (set! (.-fillStyle ctx) colour)
+     (.fillRect ctx x y 2 2))
+
+(-> (.zip sine-wave colour #(vector % %2))
+    (.take 600)
+    (.subscribe (fn [[{:keys [x y]} colour]]
+                  (fill-rect x y colour))))
+
 ;; Clear canvas before doing anything else
 (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))
 
