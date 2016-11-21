d42fa4bb233ceab2e6dd68ff978478a8a4228063
John Conti <jhconti@icloud.com>
2016-11-21 02:30:38 -0700

    Checkpoint at first cut of books exercise on rainbow sinewaves.

--- src/cljs/sinewave/core.cljs
+++ src/cljs/sinewave/core.cljs
@@ -1,5 +1,5 @@
 (ns sinewave.core
-  (:refer-clojure :exclude [time concat])
+  (:refer-clojure :exclude [time repeat])
   (:require cljsjs.rx))
 
 (enable-console-print!)
@@ -14,23 +14,25 @@
 (def red  (.map time (fn [_] "red")))
 (def blue (.map time (fn [_] "blue")))
 
-(def concat     js/Rx.Observable.concat)
+(def rx-concat     js/Rx.Observable.concat)
 (def defer      js/Rx.Observable.defer)
 (def from-event js/Rx.Observable.fromEvent)
+(def of         js/Rx.Observable.of)
+(def repeat     js/Rx.Observable.repeat)
 
 (def mouse-click (from-event canvas "click"))
 
 (def cycle-colour
-     (concat (.takeUntil red mouse-click)
-             (defer #(concat (.takeUntil blue mouse-click)
-                             cycle-colour))))
+     (rx-concat (.takeUntil red mouse-click)
+                (defer #(rx-concat (.takeUntil blue mouse-click)
+                                   cycle-colour))))
 
 (defn deg-to-rad [n]
   (* (/ Math/PI 180) n))
 
 (defn sine-coord [x]
   (let [sin (Math/sin (deg-to-rad x))
-        y (- 100 (* sin 90))]
+        y   (- 100 (* sin 90))]
       {:x x
        :y y
        :sin sin}))
@@ -38,23 +40,52 @@
 (def sine-wave
   (.map time sine-coord))
 
-(def colour (.map sine-wave
-                  (fn [{:keys [sin]}]
-                    (if (< sin 0)
-                      "red"
-                      "blue"))))
-
 (defn fill-rect [x y colour]
      (set! (.-fillStyle ctx) colour)
      (.fillRect ctx x y 2 2))
 
-(-> (.zip sine-wave cycle-colour #(vector % %2))
+;;
+;; static rainbow
+;;
+
+(def static-rainbow
+  (-> ["#FF0000" "#FF7F00" "#FFFF00" "#00FF00" "#0000FF" "#4B0082" "#9400D3"]
+      clj->js
+      js/Rx.Observable.from
+      (.zip time (fn [color _] color))
+      .repeat))
+
+;;
+;; continuous rainbow (ref. http://krazydad.com/tutorials/makecolors.php)
+;;
+
+(defn rgb->color [r g b]
+  (let [rgb-str #(str "rgb(" % ")")]
+    (->> [r g b] (map Math/round) (interpose ",") (apply str) rgb-str)))
+
+(defn sin->byte-value [x] (-> x (+ 1) (* 127.5) Math/round))
+
+(defn time->sin [freq phase t]
+  (-> t (* freq) (+ phase) deg-to-rad Math/sin))
+
+(def continuous-rainbow
+  (let [shifted #(partial time->sin 8 %)
+        byte-obs-shifted #(-> time (.map (shifted %)) (.map sin->byte-value))
+        r (byte-obs-shifted 0)
+        g (byte-obs-shifted 120)
+        b (byte-obs-shifted 240)]
+    (.zip r g b rgb->color)))
+
+;;
+;; main
+;;
+
+(defn po [obs] (.subscribe obs #(.log js/console %)))
+
+(-> (.zip sine-wave continuous-rainbow #(vector % %2))
     (.take 600)
     (.subscribe (fn [[{:keys [x y]} colour]]
                   (fill-rect x y colour))))
 
 ;; Clear canvas before doing anything else
 (.clearRect ctx 0 0 (.-width canvas) (.-height canvas))
-
-;; original template code
-;(set! (.-innerHTML (js/document.getElementById "app")) "<h1>Hello Chestnut!</h1>")
